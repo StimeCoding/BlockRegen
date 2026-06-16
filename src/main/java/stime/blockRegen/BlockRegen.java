0f1ef7a37cfdf614ff4df00f58306785ecd37f42
stime <samuel.gron@gmx.de>
2026-06-16 22:43:55 +0200

    does this work lol

---
+++ src/main/java/stime/blockRegen/BlockRegen.java
@@ -0,0 +1,58 @@
+package stime.blockRegen;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockRegen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        FileConfiguration file = this.getConfig();
+
+        getServer().getPluginManager().registerEvents(
+                new Read(file,this),
+                this
+        );
+    }
+
+
+    /*
+
+    FORMAT
+
+    <block_name>:
+        time: [int]
+        drops:
+            <item_name>:
+                amount: [int]
+                max: [int]
+                min: [int]
+                chance: [double]
+                fortune:
+                    amount: [int]
+                    max: [int]
+                    min: [int]
+                    chance: [double]
+           <item_name>:
+                amount: [int]
+                max: [int]
+                min: [int]
+                chance: [double]
+                overwrite: [boolean]
+                fortune:
+                    amount: [int]
+                    max: [int]
+                    min: [int]
+                    chance: [double]
+
+    Under drops all sections and the drops section are optional, and will be replaced by default values listed.
+    The presence of "amount" will overwrite both min and max.
+    Default for the drops section are: chance: 1.0, amount: 1 (sets both min and max too if all are absent)
+    Default for the fortune section are: chance: 0.0, amount: 0 (sets both min and max too)
+    Overwrite makes it replace ALL other possible drops if dropped
+    */
+}
---
+++ src/main/java/stime/blockRegen/Read.java
@@ -0,0 +1,115 @@
+package stime.blockRegen;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+class Read implements Listener {
+
+    private final FileConfiguration config;
+    private final BlockRegen plugin;
+    private final Random random = new Random();
+
+    public Read(FileConfiguration config, BlockRegen plugin){
+        this.config = config;
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockHit(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (event.getAction() != Action.LEFT_CLICK_BLOCK) return;
+
+        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
+
+        assert event.getClickedBlock() != null;
+        String blockname = event.getClickedBlock().getType().getKey().getKey();
+        if(!(config.getKeys(false).contains(blockname))) return;
+
+
+        assert event.getClickedBlock() != null;
+        if (!event.getClickedBlock().isPreferredTool(event.getPlayer().getInventory().getItemInMainHand())) event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event){
+        Player player = event.getPlayer();
+
+        if (player.getGameMode().equals(GameMode.CREATIVE)) return;
+
+        String blockname = event.getBlock().getType().getKey().getKey();
+        if(!(config.getKeys(false).contains(blockname))) return;
+
+        event.setCancelled(true);
+
+        //block Regen Logic
+
+        ConfigurationSection blockConfigData = config.getConfigurationSection(blockname);
+        int time = blockConfigData.getInt("time");
+
+        Material type = event.getBlock().getType();
+
+        if (time > 0) {
+            Bukkit.getScheduler().runTaskLater(plugin, () -> {
+                event.getBlock().setType(type);
+            }, time);
+
+            event.getBlock().setType(Material.BEDROCK);
+        }
+
+        //block drops Logic
+
+        if (!event.getBlock().isPreferredTool(player.getInventory().getItemInMainHand())) return;
+
+        ConfigurationSection blockDropsConfigData = blockConfigData.getConfigurationSection("drops");
+
+        if (blockDropsConfigData != null) {
+            ArrayList<ItemStack> drops = new ArrayList<>();
+
+            for (String itemName : blockDropsConfigData.getKeys(false)) {
+                ConfigurationSection itemSection = blockDropsConfigData.getConfigurationSection(itemName);
+                if (itemSection == null) continue;
+                Material item = Material.matchMaterial(itemName);
+
+                double chance = itemSection.getDouble("chance", 1.0);
+                int min = itemSection.getInt("amount", itemSection.getInt("min", 1));
+                int max = itemSection.getInt("amount", itemSection.getInt("max", 1));
+                boolean overwrite = itemSection.getBoolean("overwrite", false);
+
+                ConfigurationSection fortuneSection = itemSection.getConfigurationSection("fortune");
+                if (fortuneSection != null && player.getInventory().getItemInMainHand().getItemMeta().hasEnchant(Enchantment.FORTUNE)) {
+                    int fortune = player.getInventory().getItemInMainHand().getItemMeta().getEnchants().get(Enchantment.FORTUNE);
+
+                    chance += fortuneSection.getInt("chance", 0) * fortune;
+                    min += fortuneSection.getInt("amount", fortuneSection.getInt("min", 0));
+                    max += fortuneSection.getInt("amount", fortuneSection.getInt("max", 0));
+                }
+
+                if (random.nextDouble() > Math.min(chance, 1)) return;
+                int amount = random.nextInt(min, max + 1);
+
+                if (overwrite) drops.clear();
+                drops.add(new ItemStack(item, amount));
+
+                if (overwrite) break;
+            }
+            for (ItemStack drop : drops) player.getInventory().addItem(drop);
+        }
+    }
+}
